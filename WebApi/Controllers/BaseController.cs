a0f3412194bb971494c1b8cbb34b277f972214e6
Mehmet Karakuş <imeka4k@gmail.com>
2023-12-07 23:53:38 +0300

    update

---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class BaseController:ControllerBase
+{
+    private IMediator? _mediator;
+
+    protected IMediator? Mediator => _mediator??= HttpContext.RequestServices.GetService<IMediator>();
+
+    //eğer bir null mediator yoksa onu döndür eğer null ise git ioc ortamına bak IMediatorun karşılığını döndür
+}
